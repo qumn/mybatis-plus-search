ad0d0e46d7c5e4fca714f01ac46218b726679f12
qumn <wang247657@163.com>
2023-09-13 14:19:52 +0800

    cheat: add maven publish plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,15 @@
 plugins {
     kotlin("jvm") version "1.9.0"
+    `maven-publish`
+    signing
 }
 
-group = "xyz.qumn"
-version = "1.0-SNAPSHOT"
+group = "io.github.qumn"
+version = "1.6-beta"
 val kotestVersion = "5.7.2"
 val h2Version = "2.2.222"
+val springVersion = "5.3.29"
+
 
 repositories {
     mavenCentral()
@@ -16,11 +20,16 @@ dependencies {
     testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
     testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
     testImplementation("io.kotest:kotest-property:$kotestVersion")
+    testImplementation("com.h2database:h2:$h2Version")
     implementation("com.baomidou:mybatis-plus:3.5.3.2")
     // because the mybatis dependent on mybatis plus
-    implementation("org.springframework:spring-core:5.3.29")
-    implementation("com.h2database:h2:$h2Version")
+    implementation("org.springframework:spring-core:$springVersion")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+}
+
+tasks.register<Jar>("javadocJar") {
+    from(tasks["javadoc"])
+    archiveClassifier.set("javadoc")
 }
 
 tasks.withType<Test>().configureEach {
@@ -29,4 +38,58 @@ tasks.withType<Test>().configureEach {
 
 kotlin {
     jvmToolchain(8)
+}
+val sourcesJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("sources")
+    from(sourceSets.main.get().allSource)
+}
+
+publishing {
+    publications {
+        register("mavenJava", MavenPublication::class) {
+            from(components["java"])
+            artifact(sourcesJar)
+            artifact(tasks["javadocJar"]) {
+                classifier = "javadoc"
+            }
+
+            pom {
+                name.set("mybatis plus search")
+                description.set("a useful tool for mybatis plus search")
+                url.set("https://github.com/qumn/mybatis-plus-search")
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("qumn")
+                        name.set("Wang ZiJian")
+                        email.set("2476573497@qq.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/qumn/mybatis-plus-search.git")
+                    developerConnection.set("scm:git:ssh://github.com/qumn/mybatis-plus-search.git")
+                    url.set("https://github.com/qumn/mybatis-plus-search")
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            credentials {
+                username = System.getenv("OSSRH_USERNAME") ?: System.getProperty("OSSRH_USERNAME")
+                password = System.getenv("OSSRH_PASSWORD") ?: System.getProperty("OSSRH_PASSWORD")
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["mavenJava"])
 }
